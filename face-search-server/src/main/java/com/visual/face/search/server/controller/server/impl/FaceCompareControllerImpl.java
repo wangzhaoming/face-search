1dafa9f0e1514a54857f8af00988fc29e2fdbc40
divenswu <divenswu@163.com>
2022-03-17 18:57:55 +0800

    update:添加人脸比对1：1的接口服务

---
+++ face-search-server/src/main/java/com/visual/face/search/server/controller/server/impl/FaceCompareControllerImpl.java
@@ -0,0 +1,15 @@
+package com.visual.face.search.server.controller.server.impl;
+
+import com.visual.face.search.server.controller.server.api.FaceCompareControllerApi;
+import com.visual.face.search.server.domain.common.ResponseInfo;
+import com.visual.face.search.server.domain.request.FaceCompareReqVo;
+import com.visual.face.search.server.domain.response.FaceCompareRepVo;
+
+public class FaceCompareControllerImpl implements FaceCompareControllerApi {
+
+    @Override
+    public ResponseInfo<FaceCompareRepVo> faceCompare(FaceCompareReqVo compareReq) {
+        return null;
+    }
+
+}
